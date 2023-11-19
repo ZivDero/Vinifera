2f5a6463233aa6a7f2e0a221efeabd1b8b61e3c9
Rampastring <rampastring@outlook.com>
2023-11-19 14:12:22 +0200

    Prevent InvisibleInGame=yes buildings from claiming wall ownership

--- src/extensions/cell/cellext_hooks.cpp
+++ src/extensions/cell/cellext_hooks.cpp
@@ -28,6 +28,8 @@
 #include "cellext_hooks.h"
 #include "cellext_const.h"
 #include "tibsun_globals.h"
+#include "building.h"
+#include "buildingtype.h"
 #include "session.h"
 #include "rules.h"
 #include "iomap.h"
@@ -313,6 +315,44 @@ passes_check:
 
 
 /**
+ *  Prevents buildings with InvisibleInGame=yes from claiming wall ownership.
+ * 
+ *  Author: Rampastring
+ */
+DECLARE_PATCH(_CellClass_Update_Wall_Owner_Skip_Invisible_Buildings)
+{
+	GET_REGISTER_STATIC(BuildingClass*, building, esi);
+
+	/**
+     *  Stolen bytes/code.
+	 *  Skip the building if it is not active.
+     */
+	if (!building->IsActive) {
+		JMP(0x0045321C);
+	}
+
+	/**
+     *  Skip the building if it is invisible. If so, it is most likely a light post
+	 *  or other "decorative" object that should not affect wall ownership.
+     */
+	if (building->Class->IsInvisibleInGame) {
+		JMP(0x0045321C);
+	}
+
+	/**
+     *  Restore value of "this" pointer to ecx register just in case the compiler
+	 *  decided to use it above.
+     */
+	_asm { mov  ecx, [esp] }
+
+	/**
+     *  Continue to further checks in the wall claiming logic.
+     */
+	JMP(0x004531EB);
+}
+
+
+/**
  *  Main function for patching the hooks.
  */
 void CellClassExtension_Hooks()
@@ -323,4 +363,5 @@ void CellClassExtension_Hooks()
     Patch_Jump(0x00455130, &_CellClass_Draw_Fog_Patch);
     Patch_Jump(0x004596C0, &CellClassExt::_Can_Tiberium_Germinate);
     Patch_Jump(0x0045B0D0, &CellClassExt::_Can_Place_Veins);
+	Patch_Jump(0x004531E4, &_CellClass_Update_Wall_Owner_Skip_Invisible_Buildings);
 }
