1d93d3435343a9d6e14017ff8718f24b1796e924
ZivDero <kirill.andriiashin@gmail.com>
2024-08-19 23:42:28 +0300

    Mention function where the jump is

--- src/extensions/house/houseext_hooks.cpp
+++ src/extensions/house/houseext_hooks.cpp
@@ -1681,6 +1681,6 @@ void HouseClassExtension_Hooks()
     Patch_Jump(0x004CB6C1, &_HouseClass_Enable_SWs_Check_For_Building_Power);
     Patch_Jump(0x004C0F87, &_HouseClass_AI_Raise_Money_Fix_Memory_Corruption);
     Patch_Jump(0x004BE218, &_HouseClass_Begin_Production_Check_For_Unallowed_Buildables);
-    Patch_Jump(0x004BC023, 0x004BC102); // Skip checking the owner of the MCV when building buildings
+    Patch_Jump(0x004BC023, 0x004BC102); // Skip checking the owner of the MCV when building buildings in HouseClass::Can_Build
     // Patch_Jump(0x004C10E8, &_HouseClass_AI_Building_Intercept);
 }
